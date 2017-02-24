d7536b0a326649ef608b95cc5a03be32c72b6997
sammyjp <sammy.petros@btinternet.com>
2017-02-24 15:35:43 +0000

    Player movement and boundaries created

--- SATO_game_project/Assets/Scripts/PlayerController.cs
+++ SATO_game_project/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public float PlayerSpeed;
+    public Boundary PlayerBoundary;
+
+    public Rigidbody Player_rb;
 
 	// Use this for initialization
-	void Start () {
-		//TODO Add event listeners for player input (move/ fire)
-		//TODO Just checking if it considers me a contributor now
-		
-	}
+	void Start ()
+    {
+        Player_rb = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		//TODO Check if keys are still held down
-		
+	void Update ()
+    {
+
 	}
+
+    // FixedUpdate used for physics
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Player_rb.velocity = movementVector * PlayerSpeed;
+
+        Player_rb.position = new Vector3
+        (
+            Mathf.Clamp(Player_rb.position.x, PlayerBoundary.xMin, PlayerBoundary.xMax),
+            0.0f,
+            Mathf.Clamp(Player_rb.position.z, PlayerBoundary.zMin, PlayerBoundary.zMax)
+        );
+    }
+}
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
 }
