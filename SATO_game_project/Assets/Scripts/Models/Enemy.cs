ad84633c730a1fa3b372bbfea4f1e9eaa637d1d3
calinbuzila <buzila_calin@yahoo.com>
2017-02-25 23:22:59 +0000

    -added single enemy spawn onStart() - small tweaks in code

---
+++ SATO_game_project/Assets/Scripts/Models/Enemy.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public class Enemy
+    {
+        #region properties and variables
+
+        private static int nrOfEnemies = 0;
+        public int NrOfEnemies
+        {
+            get
+            {
+                return nrOfEnemies;
+            }
+        }
+
+        private float positionScaleX;
+        private float positionScaleZ;
+        public static List<Dictionary<float, float>> positionings;
+
+        public float PositionScaleX
+        {
+            get
+            {
+                return positionScaleX;
+            }
+            set
+            {
+                positionScaleX = value;
+            }
+        }
+        public float PositionScaleZ
+        {
+            get
+            {
+                return positionScaleZ;
+            }
+            set
+            {
+                positionScaleZ = value;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Enemy()
+        {
+
+        }
+
+        /// <summary>
+        /// increment the number of enemies, the static field is holding the number of enemies that were spawned
+        /// </summary>
+        public void increaseEnemies()
+        {
+            nrOfEnemies += 1;
+        }
+
+
+    }
+}
---
+++ SATO_game_project/Assets/UnityTestTools/UnitTesting/Editor/EnemyTests.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using Assets.Scripts;
+
+[TestFixture]
+public class EnemyTests
+{
+
+    [Test]
+    public void EditorTest()
+    {
+        //Arrange
+        var gameObject = new GameObject();
+
+        //Act
+        //Try to rename the GameObject
+        var newGameObjectName = "My game object";
+        gameObject.name = newGameObjectName;
+
+        //Assert
+        //The object has a new name
+        Assert.AreEqual(newGameObjectName, gameObject.name);
+    }
+
+    /// <summary>
+    /// Test if a single enemy exists 
+    /// </summary>
+    [Test]
+    public void MinimumSpawningEnemies()
+    {
+        Enemy enemy = new Enemy();
+        Debug.Log(enemy.NrOfEnemies);
+        Assert.That(enemy.NrOfEnemies, Is.GreaterThanOrEqualTo(1));
+       
+    }
+
+    /// <summary>
+    /// Test for enemies to be spawned only in the MainBoundary coordinates divided by 2: 19.5 and 10.0
+    /// if it returns true then the test failed
+    /// </summary>
+    /// <param name="scaleX"></param>
+    /// <param name="scaleZ"></param>
+    /// <param name="expectedX"></param>
+    /// <param name="expectedZ"></param>
+    [TestCase(13.0f,13.0f,19.5f,10.0f)]
+    public void SpawnInDesignatedArea(float scaleX, float scaleZ, float expectedX, float expectedZ)
+    {
+        Enemy enemy = new Enemy();
+        enemy.PositionScaleX = scaleX;
+        enemy.PositionScaleZ = scaleZ;
+
+        Assert.True(enemy.PositionScaleX >=0 &&enemy.PositionScaleX <= expectedX);
+        Assert.True(enemy.PositionScaleZ >= 0 && enemy.PositionScaleZ <= expectedZ);
+
+    }
+
+    /// <summary>
+    /// Test for enemy if it is correctly positioned
+    /// </summary>
+    [Test]
+    public void SpawnInDesignatedAreaFromObj()
+    {
+        Enemy enemy = new Enemy();
+
+        Debug.Log(enemy.PositionScaleX);
+        Assert.True(enemy.PositionScaleX >= 0 && enemy.PositionScaleX <= 19.5f);
+        Assert.True(enemy.PositionScaleZ >= 0 && enemy.PositionScaleZ <= 10.0f);
+       
+    }
+
+   
+}
