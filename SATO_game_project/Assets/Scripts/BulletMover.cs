77bc14a5d29b6219dda7e9ffaeab18d4ca356b9b
Ahkrin Descol <amorley525@hotmail.co.uk>
2017-02-25 23:37:09 +0000

    Added bullet script.  Currently it does nothing.

---
+++ SATO_game_project/Assets/Scripts/BulletMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+	public float speed;
+	protected const Rigidbody BULLET_RB = GetComponent<Rigidbody>();
+
+	void Start () {
+		
+	}
+
+	//TODO Handle collision with enemy
+	//TODO Handle collision with barrier
+	//TODO Handle moving on spawn
+}
