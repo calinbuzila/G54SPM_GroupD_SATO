c6db9fb85aa19a672fbf82b5ea2e82e090d10245
calinbuzila <buzila_calin@yahoo.com>
2017-02-25 16:06:27 +0000

    - added main boundary and trigger for it

---
+++ SATO_game_project/Assets/Scripts/MainBoundary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBoundary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Destroys object that passes the main boundary
+    /// </summary>
+    /// <param name="other"> Collider objects from scene that come into contact with the main boundary</param>
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Destroy_MainBoundaryExceeded!!!");
+        Destroy(other.gameObject);
+    }
+}
