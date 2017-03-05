a802240fea2d50809e607f42bc154aa251c8e751
Ahkrin Descol <amorley525@hotmail.co.uk>
2017-03-05 18:28:05 +0000

    Added more score tests.

--- SATO_game_project/Assets/Editor/ScoreTests.cs
+++ SATO_game_project/Assets/Editor/ScoreTests.cs
@@ -15,12 +15,30 @@ public class ScoreTests {
 	}
 
 	/// <summary>
+	/// Checks that the score cannot ever go into the negative through LevelController.SetScore(int);
+	/// </summary>
+	/// <param name="setToValue"></param>
+	[TestCase(-1)]
+	[TestCase(-20)]
+	[TestCase(-12345)]
+	[TestCase(25)]
+	[TestCase('B')]
+	public void ScoreNeverGoesNegativeThroughSetScore(int setToValue)
+	{
+		LevelController levelController = new LevelController ();
+		levelController.SetScore (setToValue);
+		Assert.IsTrue (levelController.GetScore () >= 0);
+	}
+
+	/// <summary>
 	/// Checks that the score cannot ever go into the negative through LevelController.AddToScore(int);
 	/// </summary>
 	/// <param name="initialValue"></param>
 	/// <param name="addedValue">
 	[TestCase(0, -1)]
-	public void ScoreNeverGoesNegative(int initialValue, int addedValue)
+	[TestCase(20, -40)]
+	[TestCase(5, -90)]
+	public void ScoreNeverGoesNegativeThroughAddToScore(int initialValue, int addedValue)
 	{
 		LevelController levelController = new LevelController ();
 
@@ -28,5 +46,24 @@ public class ScoreTests {
 		levelController.AddToScore (addedValue);
 
 		Assert.IsTrue (levelController.GetScore () >= 0);
+	}
+
+	/// <summary>
+	/// Checks that the player may never exceed the possible score limit 
+	/// through incrementation, and that the value does not cause undetermined behaviour.
+	/// </summary>
+	/// <param name="initialValue">
+	[TestCase(20)]
+	[TestCase(int.MaxValue)]
+	[TestCase(50513)]
+	public void ScoreDoesNotExceedLimitThroughIncrementation(int initialValue)
+	{
+		LevelController levelController = new LevelController ();
+
+		levelController.SetScore (initialValue);
+		levelController.IncrementScore ();
+		int newValue = levelController.GetScore ();
+
+		Assert.IsTrue (newValue == initialValue + 1 || newValue == int.MaxValue);
 	}
 }
