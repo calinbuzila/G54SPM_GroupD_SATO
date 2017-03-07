52772984321b0f9f8f116270e71195615f13b060
calinbuzila <buzila_calin@yahoo.com>
2017-03-07 10:39:11 +0000

    * modified Models to ObjectDataHandlers

---
+++ SATO_game_project/Assets/Scripts/ObjectDataHandlers/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Enemy
+    {
+        #region properties and variables
+
+        public static int NrOfEnemies = 0;
+        public static float BoundaryX;
+        public static float BoundaryZ;
+
+        public static float PositionScaleX;
+        public static float PositionScaleZ;
+        private string name;
+        public static Dictionary<float, float> positionings;
+
+       
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+      
+
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Enemy()
+        {
+            positionings = new Dictionary<float, float>();
+        }
+
+        /// <summary>
+        /// increment the number of enemies, the static field is holding the number of enemies that were spawned
+        /// </summary>
+        public void increaseEnemies()
+        {
+            NrOfEnemies += 1;
+        }
+
+        public void decreaseEnemies()
+        {
+            NrOfEnemies -= 1;
+        }
+
+    }
+}
