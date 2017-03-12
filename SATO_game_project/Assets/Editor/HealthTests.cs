3f5e782ce8be420892869ce18daebc0546504830
Ahkrin Descol <amorley525@hotmail.co.uk>
2017-03-12 14:17:32 +0000

    Added skeleton tests for player health and lives.

---
+++ SATO_game_project/Assets/Editor/HealthTests.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class HealthTests {
+
+	/// <summary>
+	/// Checks that the player's health initialises to 100.
+	/// </summary>
+	[Test]
+	public void HealthInitialisesToOneHundred() {
+		
+	}
+
+	/// <summary>
+	/// Checks that the player's health may not go into the negative through SetHealth.
+	/// </summary>
+	/// <param name="setToValue"></param>
+	[TestCase(-10)]
+	public void HealthNeverGoesNegativeThroughSetHealth(int setToValue)
+	{
+		
+	}
+
+	/// <summary>
+	/// Checks that the player's health may not go into the negative through AddToHealth.
+	/// </summary>
+	/// <param name="startingHealth"></param>
+	/// <param name="affectingValue"></param>
+	[TestCase(100, -110)]
+	public void HealthNeverGoesNegativeThroughAddToHealth(int startingHealth, int affectingValue)
+	{
+		
+	}
+
+	/// <summary>
+	/// Checks that the player's health may not go above 100 through SetHealth.
+	/// </summary>
+	/// <param name="setToValue"></param>
+	[TestCase(110)]
+	public void HealthNeverExceedsLimitThroughSetHealth(int setToValue)
+	{
+
+	}
+
+	/// <summary>
+	/// Checks that the player's health may not go above 100 through AddToHealth. 
+	/// </summary>
+	/// <param name="startingHealth"></param>
+	/// <param name="affectingValue"></param>
+	[TestCase(90, 20)]
+	public void HealthNeverExceedsLimitThroughAddToHealth(int startingHealth, int affectingValue)
+	{
+		
+	}
+
+}
---
+++ SATO_game_project/Assets/Editor/LivesTests.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class LivesTests {
+
+	/// <summary>
+	/// Checks that the player's lives initialise to three.
+	/// </summary>
+	[Test]
+	public void LivesInitialiseToThree() {
+		
+	}
+
+	/// <summary>
+	/// Checks that the player's lives may be decremented correctly.
+	/// </summary>
+	/// <param name="initialValue"></param>
+	[TestCase(3)]
+	[TestCase(2)]
+	[TestCase(1)]
+	[TestCase(20)]
+	public void LivesDecrementCorrectly(int initialValue)
+	{
+
+	}
+
+	/// <summary>
+	/// Checks that the player's lives may not go into the negative.
+	/// </summary>
+	[Test]
+	public void LivesNeverGoNegativeThroughDecrementation()
+	{
+
+	}
+
+	/// <summary>
+	/// Checks like the player's lives may be incremented correctly.
+	/// </summary>
+	/// <param name="initialValue"></param>
+	[TestCase(3)]
+	[TestCase(20)]
+	public void LivesIncrementCorrectly(int initialValue)
+	{
+
+	}
+
+	/// <summary>
+	/// Lives may not increment above int.MaxValue
+	/// </summary>
+	[Test]
+	public void LivesNeverExceedIntMaxValueThroughIncrementation()
+	{
+
+	}
+}
