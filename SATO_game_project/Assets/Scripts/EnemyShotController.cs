b7ae1061b4ecfe7b2a962accdb04717d0f597b2f
Ahkrin Descol <amorley525@hotmail.co.uk>
2017-03-19 18:10:34 +0000

    Added enemy shot, and associated script.

---
+++ SATO_game_project/Assets/Scripts/EnemyShotController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotController : MonoBehaviour {
+
+	public int speed;
+	protected Rigidbody bulletRigidBody;
+
+	void Start ()
+	{
+		bulletRigidBody = GetComponent<Rigidbody> ();	
+		// Note the bullet moves left, thus minus right.
+		bulletRigidBody.velocity = -(transform.right * speed);
+	}
+}
