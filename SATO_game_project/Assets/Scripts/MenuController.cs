9f6915d5034d8a33f0dcff003bf15cdda24d647b
calinbuzila <buzila_calin@yahoo.com>
2017-05-14 19:35:08 +0100

    * player prefs getter added in MenuController

--- SATO_game_project/Assets/Scripts/MenuController.cs
+++ SATO_game_project/Assets/Scripts/MenuController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Linq;
 
 public class MenuController : MonoBehaviour
 {
@@ -12,13 +14,15 @@ public class MenuController : MonoBehaviour
     public const int DefaultDifficultyLevel = 0;
     protected int DifficultyLevel;
     private string GameDifficulty = "GameDifficulty";
+    private string PlayerHighScores = "HighScores";
 
     protected LevelController levelController;
 
     void Start()
     {
-        levelController = GameObject.FindObjectOfType<LevelController>();
-        highscoreText.text = levelController.GetHighScores();
+        //levelController = GameObject.FindObjectOfType<LevelController>();
+        //highscoreText.text = levelController.GetHighScores();
+        highscoreText.text = GetHighScoresFromPrefs();
         if (PlayerPrefs.HasKey(GameDifficulty))
         {
             DifficultyLevel = PlayerPrefs.GetInt(GameDifficulty);
@@ -51,6 +55,17 @@ public class MenuController : MonoBehaviour
             PlayerPrefs.Save();
         }
 
+    }
+
+    public string GetHighScoresFromPrefs()
+    {
+        if (PlayerPrefs.HasKey(PlayerHighScores))
+        {
+            var playerScores = PlayerPrefs.GetString(PlayerHighScores).ToString().Split(',');
+            var newListOfScores = String.Join("\n", playerScores.Select(scoreValue => scoreValue.ToString()).ToArray());
+            return newListOfScores;
+        }
+        return "";
     }
 
 }
